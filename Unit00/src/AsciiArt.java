a498a3185bb671daac5dbd1d0a045224225ecb20
Luca Candela <lucacandela1959@gmail.com>
2019-02-01 10:23:51 -0800

    Added files for unit 0 & 1

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,30 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Luca Candela \n\n\n" );
+
+		System.out.println("                /\\                " );
+		System.out.println("               /| \\               " );
+		System.out.println("              / |_ \\              " );
+		System.out.println("             [------]              " );
+		System.out.println("             |      |              ");
+		System.out.println("          |\\ | 0  0 | /|           ");
+		System.out.println("          | \\|   >  |/ |          ");
+		System.out.println("          | /| ____ |\\ |             ");
+		System.out.println("          |/ |      | \\|             ");
+		System.out.println("             [------]              ");
+		System.out.println("              \\    /              ");
+		System.out.println("               \\  /               ");
+		System.out.println("                \\/                ");
+		//add other output
+	}
+}

2d96ee3c0ab6c0bd23b604c49f2a6ce6dd78f81f
Luca Candela <lucacandela1959@gmail.com>
2019-02-01 11:12:51 -0800

    Finish StarsAndStripes.java and StarsRunner.java

--- Unit01/src/StarsAndStripes.java
+++ Unit01/src/StarsAndStripes.java
@@ -15,21 +15,35 @@ public class StarsAndStripes
       out.println("StarsAndStripes");
       printTwoBlankLines();
    }
-
+   
+   public void printCluster() {
+	      printTwentyDashes();
+	      printTwentyStars();
+	      printTwentyDashes();
+	      printTwentyStars();
+	      printTwentyDashes();
+	      printTwentyStars();
+	      printTwentyDashes();
+   }
+   
    public void printTwentyStars()
    {
+	   out.println("********************");
    }
 
    public void printTwentyDashes()
    {
+	   out.println("--------------------");
    }
 
    public void printTwoBlankLines()
    {
+	   out.println("\n");
    }
    
    public void printASmallBox()
    {	
+	   
    }
  
    public void printABigBox()
--- Unit01/src/StarsRunner.java
+++ Unit01/src/StarsRunner.java
@@ -12,8 +12,13 @@ public class StarsRunner
 {
    public static void main(String args[])
    {
+	   StarsAndStripes pen = new StarsAndStripes();
       //instantiate a StarsAndStripes object
-      
+	   pen.printTwentyDashes();
+	   pen.printCluster();
+	   pen.printTwoBlankLines();
+	   pen.printCluster();
+	   pen.printTwentyDashes();
       //call the methods needed to make the patterns on the word document
       
    }

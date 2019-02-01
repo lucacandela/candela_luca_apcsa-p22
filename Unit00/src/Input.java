02613ed2950ac788d3565f4dace3d59d3e344932
Luca Candela <lucacandela1959@gmail.com>
2019-02-01 10:31:06 -0800

    finished input.java

--- Unit00/src/Input.java
+++ Unit00/src/Input.java
@@ -5,8 +5,6 @@
 //Date -
 //Class -
 //Lab  -
-
-import static java.lang.System.*;
 import java.util.Scanner;
 
 public class Input
@@ -44,11 +42,19 @@ public class Input
 		shortOne = keyboard.nextShort();
 		
 		System.out.print("Enter a short :: ");
+		shortTwo = keyboard.nextShort();
 		//add in input for all variables
-
+		keyboard.close();
 		System.out.println();
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
+		System.out.println("double one = " + doubleOne );
+		System.out.println("double two = " + doubleTwo );
+		System.out.println("float one = " + floatOne );
+		System.out.println("float two = " + floatTwo );
+		System.out.println("short one = " + shortOne );
+		System.out.println("short two = " + shortTwo );
+		
 
 		//add in output for all variables
 

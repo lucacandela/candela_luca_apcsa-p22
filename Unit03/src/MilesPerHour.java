043743648e8b93a9df94c782c9845d5779c84bc0
Luca Candela <lucacandela1959@gmail.com>
2019-02-04 11:12:29 -0800

    complete MilesPerHour.java

--- Unit03/src/MilesPerHour.java
+++ Unit03/src/MilesPerHour.java
@@ -18,29 +18,38 @@ public class MilesPerHour
 	public MilesPerHour()
 	{
 		setNums(0,0,0);
-		mph=0.0;
+		mph=5.0;
 	}
 
 	public MilesPerHour(int dist, int hrs, int mins)
 	{
+		setNums(dist,hrs,mins);
 	}
 
 	public void setNums(int dist, int hrs, int mins)
 	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
 	}
 
 	public void calcMPH()
 	{
+		mph = hours * 60;
+		mph += minutes;
+		mph /= 60;
+		mph = distance/mph;
 	}
 
 	public void print()
 	{
+		System.out.printf(toString());
 	}
 	
 	//create a print or toString or both
 	
 	public String toString()
 	{
-		return "";
+		return distance + " miles in " + hours + " hour and " + minutes + " minutes = " + mph + " MPH.";
 	}
 }

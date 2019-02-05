1ffeac6dd0f64e7c3740d3afbafe794bba669035
Luca Candela <lucacandela1959@gmail.com>
2019-02-05 10:42:36 -0800

    completed AddSubMult.java

--- Unit04/src/AddSubMult.java
+++ Unit04/src/AddSubMult.java
@@ -9,6 +9,13 @@ public class AddSubMult
 {
 	public static double check( double a, double b )
 	{
-		return 0;
+		if (a > b){
+			return a-b;
+		}
+		else if ( b > a) {
+			return a + b;
+		}
+		else
+			return a * b;
 	}
 }

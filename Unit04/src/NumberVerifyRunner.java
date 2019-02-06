ce0967a6ecc6aed9873357a17c29f2961956addc
Luca Candela <lucacandela1959@gmail.com>
2019-02-06 11:39:55 -0800

    added another test case for NumberVerify.java

--- Unit04/src/NumberVerifyRunner.java
+++ Unit04/src/NumberVerifyRunner.java
@@ -19,6 +19,8 @@ public class NumberVerifyRunner
 		
 		out.println("5 is odd :: " + NumberVerify.isOdd(5));
 		out.println("5 is even :: " + NumberVerify.isEven(5));
+		out.println("12 is odd :: " + NumberVerify.isOdd(12));
+		out.println("12 is even :: " + NumberVerify.isEven(12));
 		
 		out.printf("Enter an integer :: ");
 		checkNum = keyboard.nextInt();

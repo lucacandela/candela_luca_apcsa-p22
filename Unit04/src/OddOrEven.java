656fd99ca9c166b7749bf086fd19d776de8e4fbb
Luca Candela <lucacandela1959@gmail.com>
2019-02-06 11:47:51 -0800

    Finish OddOrEven.java

--- Unit04/src/OddOrEven.java
+++ Unit04/src/OddOrEven.java
@@ -10,12 +10,21 @@ import static java.lang.System.*;
 
 public class OddOrEven
 {
-	public static boolean isOdd( )
+	public static boolean isOdd( int num )
 	{
-		return true;
+		return num % 2 !=0;
 	}
-	public static boolean isEven( )
+	public static boolean isEven( int num )
 	{
-		return true;
-	}	
+		return !isOdd(num);
+	}
+	
+	public static void main(String[] args) {
+		out.println("14 is odd? :: " + isOdd(14));
+		out.println("14 is even? :: " + isEven(14));
+		
+		out.println("86 is even? :: " + isEven(86));
+		
+		out.println("74 is odd? :: " + isOdd(74));
+	}
 }

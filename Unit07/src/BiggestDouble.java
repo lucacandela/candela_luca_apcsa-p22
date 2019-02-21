9057150d29a6fca9c514789e6e58a92fe7bd3281
Luca Candela <lucacandela1959@gmail.com>
2019-02-21 10:47:32 -0800

    Finish BiggestDouble

--- Unit07/src/BiggestDouble.java
+++ Unit07/src/BiggestDouble.java
@@ -7,27 +7,42 @@ import static java.lang.System.*;
 public class BiggestDouble
 {
 	private double one,two,three,four;
-
+	private double[] list;
 	public BiggestDouble()
 	{
-		this(0,0,0,0);
+		setDoubles(0,0,0,0);
+		
 	}
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a,b,c,d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
+		list = new double[] {a,b,c,d};
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double largest = 0;
+		for (int i = 0; i < 4; i++)
+			if (list[i] > largest) {
+				largest = list[i];
+
+			}
+		return largest;
 	}
+	
+	
 
 	public String toString()
 	{
-	   return "";
+	   return one + " " + two + " " + three + " " + four;
 	}
 }

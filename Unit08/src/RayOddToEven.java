b39bb170b3d267358bc63f62f84f8dd683022f75
Luca Candela <lucacandela1959@gmail.com>
2019-02-27 11:35:26 -0800

    finish RayOddToEven.java

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -5,8 +5,30 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int firstOdd = 0; int firstEven = 1;
+		int firstIndex = 0; int secondIndex = 0;
+		
+		for (int i = 0; i < ray.length-1; i++) {
+			if (ray[i] % 2 == 1) {
+				firstOdd = ray[i];
+				firstIndex = i;
+				break;
+			}
+				
+		}
+		for (int i = firstIndex; i < ray.length-1; i++) {
+			if (ray[i] % 2 == 0) {
+				firstEven = ray[i];
+				secondIndex = i;
+				break;
+			}
+		}
+		if (firstOdd == 0 || (!(firstOdd == 0) && firstEven ==1)) {
+			return -1;
+		}
+		else
+			return secondIndex - firstIndex;
 	}
 }

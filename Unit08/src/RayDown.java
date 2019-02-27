cf49af50284be776986bee4dfa966b491279112a
Luca Candela <lucacandela1959@gmail.com>
2019-02-27 11:24:53 -0800

    Finish RayDown.java
    and added test cases to DownRunner.java

--- Unit08/src/RayDown.java
+++ Unit08/src/RayDown.java
@@ -9,10 +9,17 @@ public class RayDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		int count =0;
-		for (int i = numArray.length-2; i > 0; i--) {
-			if (!(numArray[i] < numArray[i+1])) {
+		int count = 0;
+		for (int i = 0; i < numArray.length-2; i++) {
+			if (!(numArray[i] > numArray[i+1]) || (numArray[i] == numArray[i+1])) {
 				count++;
+			}
+		}
+		if (numArray.length == 2) {
+			for (int i = 0; i < 1; i++) {
+				if (!(numArray[i] > numArray[i+1]) || (numArray[i] == numArray[i+1])) {
+					count++;
+				}
 			}
 		}
 		if (count == 0)

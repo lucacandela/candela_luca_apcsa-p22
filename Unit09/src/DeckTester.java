a442d1be94261a7eca820c79f1bfb60dfa25db0c
Luca Candela <lucacandela1959@gmail.com>
2019-03-07 10:26:27 -0800

    Update DeckTester.java
    small fix in DeckTester

--- Unit09/src/DeckTester.java
+++ Unit09/src/DeckTester.java
@@ -11,7 +11,7 @@ public class DeckTester {
 		Deck deck = new Deck(new String[] {"A","B","C"}, new String[] {"Giraffes","Lions"}, new int[] {2,1,6});
 		System.out.println("Is deck empty? :: " + deck.isEmpty());
 		System.out.println("How many cards are in the deck? :: " + deck.size());
-		System.out.print("Deal me five cards. :: ");
+		System.out.println("Deal me five cards. :: ");
 		for(int i = 0; i <= 4; i++) deck.deal();
 		System.out.println(deck);
 		
@@ -19,7 +19,7 @@ public class DeckTester {
 		deck = new Deck(new String[] {"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"}, new String[] {"Hearts","Spades","Diamonds","Clubs"}, new int[] {2,3,4,5,6,7,8,9,10,10,10,10,11});
 		System.out.println("Is deck empty? :: " + deck.isEmpty());
 		System.out.println("How many cards are in the deck? :: " + deck.size());
-		System.out.print("Deal me five cards. :: ");
+		System.out.println("Deal me five cards. :: ");
 		for(int i = 0; i <= 4; i++) deck.deal();
 		System.out.println(deck);
 		
@@ -27,7 +27,7 @@ public class DeckTester {
 		deck = new Deck(new String[] {"One","Two","Three"}, new String[] {"Apples","Bannanas"}, new int[] {1,2,3});
 		System.out.println("Is deck empty? :: " + deck.isEmpty());
 		System.out.println("How many cards are in the deck? :: " + deck.size());
-		System.out.print("Deal me five cards. :: ");
+		System.out.println("Deal me five cards. :: ");
 		for(int i = 0; i <= 4; i++) deck.deal();
 		System.out.println(deck);
 	}

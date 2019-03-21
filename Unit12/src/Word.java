82432d595feab15406fdfccd153ed7dee4bcb33d
Luca Candela <lucacandela1959@gmail.com>
2019-03-20 21:07:09 -0700

    finished Word.java

--- Unit12/src/Word.java
+++ Unit12/src/Word.java
@@ -10,15 +10,31 @@ public class Word implements Comparable<Word>
 
 	public Word( String s )
 	{
+		word = s;
 	}
 
 	public int compareTo( Word rhs )
-	{		
+	{	
+		String other = (String)rhs.word;
+		
+		if (word.length() > other.length()) {
+			return 1;
+		}
+		else if (word.length()<other.length()) {
+			return -1;
+		}
+		else if (word.length() == other.length()) {
+			if (word.charAt(0) > other.charAt(0)) {
+				return 1;
+			}
+			else
+				return -1;
+		}
 		return 0;
 	}
-
+	
 	public String toString()
 	{
-		return "";
+		return word;
 	}
 }
--- Unit12/src/WordRunner.java
+++ Unit12/src/WordRunner.java
@@ -13,20 +13,25 @@ public class WordRunner
 	public static void main( String args[] ) throws IOException
 	{
 		Scanner file = new Scanner(new File("words.dat"));
-
+		
 		int size = file.nextInt();
 		file.nextLine();
 		
-
-
-
-
-
-
-
-
-
-
-
+		Word w;
+		
+		Comparable[] list = new Comparable[size];
+		for (int i = 0; i < size; i++) {
+			if (file.hasNextLine()) {
+				w = new Word(file.next());
+				
+				list[i] = w;
+			}
+		
+		}
+		Arrays.sort(list);
+		
+		for (int i = 0; i < size; i++) {
+			System.out.println(list[i]);
+		}
 	}
 }

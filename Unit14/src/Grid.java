f4be7d0137700c19bdcc0cf7d2d949b95ac74360
Luca Candela <lucacandela1959@gmail.com>
2019-03-22 11:03:09 -0700

    Completed Grid.java

--- Unit14/src/Grid.java
+++ Unit14/src/Grid.java
@@ -5,28 +5,77 @@
 public class Grid
 {
    private String[][] grid;
-
+   private String[] values;
+   private int rLength;
+   private int cLength;
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		rLength = rows;
+		cLength = cols;
+		grid = new String[rLength][cLength];
+		values = vals;
+		
+		
+		int rand;
+		
+		for (int i = 0; i < rLength; i++) {
+			for (int x = 0; x < cLength; x++) {
+				rand = (int)(Math.random() * vals.length);
+				grid[i][x] = vals[rand] ;
+			}
+		}
 	}
-
+	public String[] values() {
+		return values;
+	}
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		String out = "";
+		int count = 0;
+		int highest = 0;
+		String most = "";
+		for (int i = 0; i < vals.length; i++) {
+			count = countVals(vals[i]);
+			if (count > highest) {
+				highest = count;
+				most = vals[i];
+				
+			}
+			out += vals[i] + " count is " + count + "\n";
+		}
+		if (highest == 0)
+			return "nothing yet";
+		out += "\n" + most + " occurs the most at " + highest + " times.";
+		return out;
+		
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		for (int i = 0; i < rLength; i++) {
+			for (int x = 0; x < cLength; x++) {
+				if(grid[i][x].equals(val)) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 
 	//display the grid
 	public String toString()
 	{
 		String output="";
+		for (int i = 0; i < rLength; i++) {
+			for (int x  = 0; x < cLength; x++) {
+				output += grid[i][x] + " ";
+			}
+			output += "\n";
+		}
 		return output;
 	}
 }
--- Unit14/src/GridRunner.java
+++ Unit14/src/GridRunner.java
@@ -11,5 +11,14 @@ public class GridRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		Grid g = new Grid(20,20,new String[] {"a","b","c","7","9","x","2"});
+		
+	
+		
+		System.out.println(g);
+		
+		System.out.println("--debugging code to help see if algorithm is working-");
+		System.out.println("--these println statements can be added to the max method\n");
+		System.out.print(g.findMax(g.values()));
 	}
 }

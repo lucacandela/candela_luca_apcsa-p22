8df718050e2c04041e92d233f1ff2b3da3b2c086
Luca Candela <lucacandela1959@gmail.com>
2019-03-25 10:42:09 -0700

    Finished RecursionFunOne & Added Test Cases

--- Unit14/src/RecursionFunOne.java
+++ Unit14/src/RecursionFunOne.java
@@ -4,12 +4,28 @@
 
 import static java.lang.System.*;
 
+import java.util.List;
+
 public class RecursionFunOne
 {
 	public static int countOddDigits(int num)
 	{
-
-
+		int remainingDigits = 1;
+		String str = "" + num;
+		if(num>0) {
+			int lastDigit = num % 10;
+			if (str.length() > 1) {
+				remainingDigits = Integer.parseInt(str.substring(0, str.length()-1));
+			}
+			else {
+				remainingDigits = 0;
+			}
+			if (lastDigit % 2 == 0) {
+				return 1 + countOddDigits(remainingDigits);
+			}
+			return 0 + countOddDigits(remainingDigits);
+		}
 		return 0;
+
 	}
 }

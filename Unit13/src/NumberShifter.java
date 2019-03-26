67a24eaf25c269be76c41603173aa4d24af11a83
Luca Candela <lucacandela1959@gmail.com>
2019-03-26 11:04:45 -0700

    Finished numberShifter and runner

--- Unit13/src/NumberShifter.java
+++ Unit13/src/NumberShifter.java
@@ -12,9 +12,26 @@ public class NumberShifter
 {
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+		int[] arr = new int[size];
+		int rand;
+		for (int i = 0; i < size; i ++) {
+			rand = (int)(Math.random() * 10) + 1;
+			arr[i] = rand;
+		}
+			
+		return arr;
 	}
 	public static void shiftEm(int[] array)
 	{
+		int count = 0;
+		for (int i = 0; i < array.length; i++){
+			if (array[i] == 7) {
+				int placeHolder = array[count];
+				array[count] = 7;
+				array[i] = placeHolder;
+				count++;
+				
+			}
+		}
 	}
 }
--- Unit13/src/NumberShifterRunner.java
+++ Unit13/src/NumberShifterRunner.java
@@ -12,7 +12,10 @@ public class NumberShifterRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		//add test cases
+		int[] arr = NumberShifter.makeLucky7Array(20);
+		System.out.println(Arrays.toString(arr));
+		NumberShifter.shiftEm(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 }
 

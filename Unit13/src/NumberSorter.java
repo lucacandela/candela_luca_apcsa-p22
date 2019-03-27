e1272ef79c7db26d553465b931d42e5e052cd641
Luca Candela <CandelaL0514@sduhsd.lan>
2019-03-27 11:53:15 -0700

    Finished NumberSorter.java

--- Unit13/src/NumberSorter.java
+++ Unit13/src/NumberSorter.java
@@ -2,7 +2,9 @@
 //www.apluscompsci.com
 //Name -
 
-import java.util.Arrays; 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -10,17 +12,43 @@ import static java.lang.System.*;
 
 public class NumberSorter
 {
-	//instance variables and other methods not shown
-
+	
+	private static List<Integer> separateDigits(int number) {
+		List<Integer> n = new ArrayList<Integer>();
+		int result;
+		while(!(number == 0)) {
+			result = number % 10;
+			number = number /10;
+			n.add(result);
+		}
+		return n;
+	}
 	private static int getNumDigits(int number)
 	{
-		int count = 0;
-		return count;
+		int size = separateDigits(number).size();
+		return size;
 	}
-
+	
+	
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int[] sorted = new int[getNumDigits(number)];
+		List<Integer> n = separateDigits(number);
+		int count = 0;
+		for(Integer x : n) {
+			sorted[count] = x;
+			count++;
+		}
+		int placeHolder = 0;
+		for(int i = 0; i < sorted.length-1; i++) {
+			for(int x = 0; x < sorted.length-1; x++) {
+				if(sorted[x] > sorted[x+1]){
+					placeHolder = sorted[x+1];
+					sorted[x+1] = sorted[x];
+					sorted[x] = placeHolder;
+					}
+			}
+		}
 		return sorted;
 	}
 }

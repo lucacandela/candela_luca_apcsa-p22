f6243d157d068d3bfa0d687660547bf6a86fe59f
Luca Candela <lucacandela1959@gmail.com>
2019-04-15 10:39:30 -0700

    Update Block.java

--- Unit15-InstructionExamples/src/Block.java
+++ Unit15-InstructionExamples/src/Block.java
@@ -16,57 +16,110 @@ public class Block implements Locatable
 
 	public Block()
 	{
-
-
+		setPos(100,150);
+		setWidth(10);
+		setHeight(10);
+		setColor(Color.black);
 	}
-
-	//add other Block constructors - x , y , width, height, color
 	
+	public Block(int x, int y) {
+		setPos(x,y);
+		setWidth(10);
+		setHeight(10);
+		setColor(Color.black);
+	}
+	public Block(int x, int y,int w, int h) {
+		setPos(x,y);
+		setWidth(w);
+		setHeight(h);
+		setColor(Color.black);
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public Block(int x, int y,int w, int h, Color col) {
+		setPos(x,y);
+		setWidth(w);
+		setHeight(h);
+		setColor(col);
+	}	
 	
 	
    //add the other set methods
+	public void setPos(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+	
+	public void setX(int x) {
+		xPos = x;
+	}
+  
+	public void setY(int y) {
+		yPos = y;
+	}
    
+	public void setWidth(int w){
+		width = w;
+	}
+   
+	public void setHeight(int h) {
+		height = h;
+	}
 
-   public void setColor(Color col)
+	public void setColor(Color col)
+	{
+		color = col;
+	}
+
+	public void draw(Graphics window)
    {
-
-
-   }
-
-   public void draw(Graphics window)
-   {
-   	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+      window.setColor(color);
+      window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
 
    public void draw(Graphics window, Color col)
    {
-
-
+	   window.setColor(col);
+	   window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
    
 	public boolean equals(Object obj)
 	{
-
-
-
-
-		return false;
+		if(obj.getClass().getName().equals(this.getClass().getName())) {
+			Block other = (Block) obj;
+			if(other.getX() == getX() && other.getY() == getY() && other.getWidth() == getWidth()
+					&& other.getHeight() == getHeight() && other.getColor().equals(getColor())) {
+				return true;
+			}
+			return false;
+		}
+		else
+			return false;
 	}   
 
    //add the other get methods
-    
+	public int getX() {
+		return xPos;
+	}
+	
+	public int getY() {
+		return yPos;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
 
    //add a toString() method  - x , y , width, height, color
+	public String toString() {
+		String out = getX() + " " + getY() + " " + getWidth() + " " + getHeight() + " " + getColor();
+		return out;
+	}
 }
